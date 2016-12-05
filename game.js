acdcfe912d9f81f7d8acd6e30c884de36f12228b
William Simmons <willsimmons@gmail.com>
2016-12-05 15:13:59 -0800

    pseudo code for game flow as well as for strategic planning

---
+++ game.js
@@ -0,0 +1,13 @@
+let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+//so lets create a board, 0, 9 because that's how many spaces are in a tic tac toe board
+let turnCounter = 0;
+//this will be handy in case of a tie
+//x will go first all of the time
+let currentPlayer = 'X';
+//so from a simplistic level, i'm thinking while gameOver is false; 
+//we swicth turns between X and 0, and accept an input, that input represents their turn 
+//if it's a legal turn
+//after the turn we need to check to see if the game is over, if not, then the next player goes
+//
+
+
