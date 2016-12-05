588afe40fd6e0b2ff7e68fc16eb0a0fd804c5f7f
William Simmons <willsimmons@gmail.com>
2016-12-05 15:31:02 -0800

    working on more game logic, out of time

--- game.js
+++ game.js
@@ -1,5 +1,17 @@
 let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 //so lets create a board, 0, 9 because that's how many spaces are in a tic tac toe board
+//naive results aka game over in case of the following are all one character
+//could probably do better as a nested array, but we're out of time
+//012  horizontal
+//345
+//678
+//
+//036  vertical
+//147
+//258
+//
+//048 diagonal
+//246
 let turnCounter = 0;
 //this will be handy in case of a tie
 //x will go first all of the time
@@ -17,9 +29,11 @@ while (gameOver !== true) {
     gameOver = true;
   }
   //player plays - can be used for x or y
-  takeTurn(currentPlayer, board){
+  cont takeTurn(currentPlayer, board) {
+    console.log(board); //yes it will just show up as a numberic array, but that's for
+    //simplistic sample purposes
     console.log("It's "+ currentPlayer +"'s turn, please enter a square: ");
-    //something revolving stdin to grab the actual input
+    //something revolving stdin to grab the actual input 
     //check to see if the value is a number
       //if not, then reprompt
     //check to see if the move is valid
@@ -28,4 +42,8 @@ while (gameOver !== true) {
       //if not, then take turn with the other player
   }
   turnCounter++;
+}
+
+const checkBoard(board) {
+  //can simply do if statements for all potential board spots
 }
