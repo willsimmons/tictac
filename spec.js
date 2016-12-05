0bd39748ca1ced6f961c3144f377d38b0f7be315
William Simmons <willsimmons@gmail.com>
2016-12-05 15:22:50 -0800

    may have overplanned with the eventual tests, but need to keep track of what is going on function-wise

---
+++ spec.js
@@ -0,0 +1,15 @@
+const mocha = require('mocha');
+const expect = require('chai').expect;
+const game = require('./game.js');
+
+describe('Game', function() {
+  //i need a display board function to show the game state between plays
+  // describe('display board', function() {
+  //   it('should display the current state of the gameboard', function(done) {
+     
+  //   });
+  // });
+  //i need a 'is valid move' checking function to make sure the player can make a move
+  //i need a 'is game over 'function to see if there is a winner or a draw
+  //i need a 'turn tracker' function to determine if it's X or 0's turn
+});
